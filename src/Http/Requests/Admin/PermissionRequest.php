2ac85498e81575503c4c3fe369524d63ac812e57
wikichua <wikichua@gmail.com>
2022-05-12 19:51:11 +0800

    first commit

---
+++ src/Http/Requests/Admin/PermissionRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Wikichua\Bliss\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Wikichua\Bliss\Rules\AtLeastNotEmpty;
+use Wikichua\Bliss\Rules\AllFilledNoEmpty;
+
+class PermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                new AtLeastNotEmpty(1),
+                new AllFilledNoEmpty(),
+            ],
+            'group' => 'required',
+        ];
+    }
+}
---
+++ src/Http/Requests/Admin/ProfileSaveAvatarRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wikichua\Bliss\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileSaveAvatarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'avatar' => [
+                'required',
+                'mimes:jpeg,jpg,png',
+                'max:1024',
+                'dimensions:min_width=250,min_height=250'
+            ],
+        ];
+    }
+}
